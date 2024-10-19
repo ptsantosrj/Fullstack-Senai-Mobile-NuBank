67e5459a219dfaa8e32083dc21d3bf19c754d45d
Patrick Tavares dos Santos <ptsantosrj@gmail.com>
2024-10-18 21:30:31 -0300

    Criação do roteamento para as telas home e pix

---
+++ components/AppNavigator.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+import PixScreen from '../screens/PixScreen';
+
+const Stack = createStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Pix" component={PixScreen} />
+    </Stack.Navigator>
+  );
+}
