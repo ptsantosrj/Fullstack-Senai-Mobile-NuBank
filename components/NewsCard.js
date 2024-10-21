6a827bd1216e2bed5b3a6592b3961c2523cd9dd3
Patrick Tavares dos Santos <ptsantosrj@gmail.com>
2024-10-21 20:38:04 -0300

    Implementado scroll do flatlist no Newscard

--- components/NewsCard.js
+++ components/NewsCard.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ScrollView, View, Text } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import styles from '../styles/StyleNewsCard';
 import news from '../lists/news';
 import { useRef, useEffect } from 'react';
 import { FlatList } from 'react-native-web';
+import Animated from 'react-native-reanimated';
 
 const NewsCard = () => {
    
+  const flatListRef = useRef(null)
+  const [ currentIndex, setCurrentIndex ]= useState(0);
+
+  useEffect( () => {
+    const interval = setInterval(() => {
+      let nextIndex = currentIndex + 1;
+      if (nextIndex >= news.length) {
+        nextIndex = 0;
+      }
+      flatListRef.current.scrollToIndex({ index:nextIndex, Animated: true });
+      setCurrentIndex(nextIndex);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [currentIndex, news.length]
+  );
+
     return (
         <FlatList
         data={news}
         horizontal
         keyExtractor={(item) => item.id}
         showsHorizontalScrollIndicator={false}
-        //contentContainerStyle={styles.newsCard}
+        //contentContainerStyle={styles.carouselContainer}
+        ref={flatListRef}
+
         renderItem={({ item }) => (
             <View key={item.id} style={styles.newsCard}>
               <View style={styles.carouselContainer}>
